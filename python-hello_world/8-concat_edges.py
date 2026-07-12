8d19be6f161eaff19982124ac662579ba4c3af7e
modek2013 <2013kulbaibe@gmail.com>
2026-07-12 22:55:37 +0100

    Fix output and pycodestyle errors in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 str = str[39:65] + str[72:77] + str[:6]
 print(str)
