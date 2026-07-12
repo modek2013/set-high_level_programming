f23bf74f552e2039be7145f98385182824e68d50
modek2013 <2013kulbaibe@gmail.com>
2026-07-12 22:44:36 +0100

    corrected my code syntax

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
+str = str[39:65] + str[72:77] + str[:6]
 print(str)
